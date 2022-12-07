0a29fa7bf5f8a904dde34b2795f2dc9d6058282a
한승재 <51234550+kimcumjee@users.noreply.github.com>
2022-12-08 01:02:19 +0900

    add project

---
+++ umemo/umemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  umemoApp.swift
+//  umemo
+//
+//  Created by 한승재 on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct umemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
