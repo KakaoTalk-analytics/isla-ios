1dd1cfad0eacd091d66ba93c3d796e7c351a2d29
한승재 <51234550+kimcumjee@users.noreply.github.com>
2023-01-26 19:16:37 +0900

    앱아이콘 추가, search field 추가, 폴더 생성 기능 구현 중

--- umemo/ContentView.swift
+++ umemo/ContentView.swift
@@ -21,66 +21,62 @@ struct MockupHeader: Identifiable {
 }
 
 struct ContentView: View {
-    var list: Array<MockupCellData> = [
-        MockupCellData(name: "모든 iCloud", count: "100", icon: "folder"),
-        MockupCellData(name: "쥬라기 공원", count: "101", icon: "folder"),
-        MockupCellData(name: "매트릭스", count: "102", icon: "folder"),
-        MockupCellData(name: "스타워즈", count: "103", icon: "folder"),
-        MockupCellData(name: "탑건", count: "104", icon: "folder"),
-        MockupCellData(name: "반지의제왕", count: "105", icon: "folder")
-    ]
-    
-    var headerList: Array<MockupHeader> = [
+    var folderList: Array<MockupHeader> = [
         MockupHeader(name: "iCloud", cellData: [
             MockupCellData(name: "모든 iCloud", count: "100", icon: "folder"),
             MockupCellData(name: "쥬라기 공원", count: "101", icon: "folder"),
             MockupCellData(name: "매트릭스", count: "102", icon: "folder"),
             MockupCellData(name: "스타워즈", count: "103", icon: "folder"),
             MockupCellData(name: "탑건", count: "104", icon: "folder"),
-            MockupCellData(name: "반지의제왕", count: "105", icon: "folder")
+            MockupCellData(name: "반지의제왕", count: "105", icon: "folder"),
+            MockupCellData(name: "최근 삭제된 항목", count: "8", icon: "trash")
         ]),
-        MockupHeader(name: "영화", cellData: [
-            MockupCellData(name: "모든 iCloud", count: "100", icon: "folder"),
-            MockupCellData(name: "쥬라기 공원", count: "101", icon: "folder"),
-            MockupCellData(name: "매트릭스", count: "102", icon: "folder"),
-            MockupCellData(name: "스타워즈", count: "103", icon: "folder"),
-            MockupCellData(name: "탑건", count: "104", icon: "folder"),
-            MockupCellData(name: "반지의제왕", count: "105", icon: "folder")
-        ])
     ]
-    
+    @State private var folderName: String = ""
+    @State private var isFolderPresenting: Bool = false
+    @State private var isCreatePresenting: Bool = false
     var body: some View {
         NavigationView {
-            List(headerList) {data in
-                Section {
-                    ForEach(data.cellData) { list in
-                        FolderCell(title: list.name, count: list.count, icon: list.icon)
-                    }
-                } header: {
-                    Text(data.name)
-                }
-            }.listStyle(.automatic).navigationTitle(Text("Folders"))
-             .navigationBarTitleDisplayMode(.automatic)
-             .toolbar {
+            VStack {
+                List(folderList) {data in
+                    Section {
+                        ForEach(data.cellData) { list in
+                            NavigationLink(destination: MemoList(title: list.name)) {
+                                FolderCell(title: list.name, count: list.count, icon: list.icon)
+                            }
+                        }
+                    } header: {
+                        Text("iCloud").font(.headline).foregroundColor(Color.black).textCase(nil)
+                    }.accentColor(Color(hex: "#dba634"))
+                }.listStyle(.automatic).navigationTitle(Text("Folders")).navigationBarTitleDisplayMode(.automatic).overlay(SearchWidget(text: $folderName, searchResultsContent: {
+                    
+                }).frame(width: 0, height: 0))
+            }.toolbar {
                  ToolbarItem(placement: .automatic) {
                      Button {
-
+                         
                      } label: {
                          Text("Edit").foregroundColor(Color(hex: "#dba634"))
                      }
+                     
                  }
                  
                  ToolbarItemGroup(placement: .bottomBar) {
                      Button(action: {
-                         
+                         self.isFolderPresenting.toggle()
                      }) {
                         Image(systemName: "folder.badge.plus").foregroundColor(Color(hex: "#dba634"))
+                     }.sheet(isPresented: $isFolderPresenting) {
+                         Text("폴더 생성 View")
                      }
                      
+                     
                      Button(action: {
-                         
+                         self.isCreatePresenting.toggle()
                      }) {
                         Image(systemName: "square.and.pencil").foregroundColor(Color(hex: "#dba634"))
+                     }.sheet(isPresented: $isCreatePresenting) {
+                         Text("메모 작성 View")
                      }
                  }
              }
